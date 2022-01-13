cfba1f5cca0c60a7fe70ddb8034e7a6988d974d5
RamyaAPN <97675783+RamyaAPN@users.noreply.github.com>
2022-01-13 22:16:02 +0000

    Add files via upload
    Scenario:
    
    This is to automate the fields of Request call back sectionof Mortgage Advice Bureau contact us webpage.
    1. The fields have been automated to get entered with the text values using sendKeys("");
    2. The Static dropdown option has been automated to receive any of the options using Select class and .ByValue() method.
    3. Finally the check box to accept to the terms and conditions have been checked but because of the recaptcha interruption, not able to execute the check box validation.
    4. As the instruction has been given only to do the above and so the 'Submit' option has not been clicked.

---
+++ reqCall.java
@@ -0,0 +1,44 @@
+package demoPack;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class reqCall {
+	public static void main(String[] args) {
+		// Filling out the details of the Request call back section fields in MAB website
+		
+		System.setProperty("webdriver.chrome.driver", "C:////work////chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		//driver.manage().timeouts().implicitlyWait(2000,TimeUnit.SECONDS);
+		driver.get("https://www.mortgageadvicebureau.com/contact-us");
+		driver.findElement(By.xpath("//button[@class='cookies__button js-cookies-trigger']")).click();
+		//driver.findElement(By.id("callbackModalBtn")).click();
+		driver.findElement(By.id("FirstName")).sendKeys("First");
+		driver.findElement(By.id("LastName")).sendKeys("User");
+		driver.findElement(By.id("EmailAddress")).sendKeys("firstuser@gmail.com");
+		driver.findElement(By.id("PhoneNumber")).sendKeys("00000000");
+		driver.findElement(By.id("BestTimeToCallYou")).sendKeys("10 am");
+		//Static dropdown selection
+		Select sl= new Select(driver.findElement(By.id("ReasonForEnquiry")));
+		sl.selectByValue("Home Mover");
+        //Explicit wait for finding the check box element but the below cant be proceeded due to recaptcha interruption
+		WebDriverWait w= new WebDriverWait(driver,100);
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.id("OptInEmail")));
+		//WebElement ch= driver.findElement(By.id("OptInEmail"));
+		//Checking and unchecking the check box
+		driver.findElement(By.id("OptInEmail")).click();
+		driver.findElement(By.id("OptInEmail")).click();
+		driver.close();
+		
+      
+	}
+
+}
